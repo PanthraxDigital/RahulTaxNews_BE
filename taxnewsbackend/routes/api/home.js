a7a02afb3bcce1999529b7677fcb25a2a26df0be
yashaswi maharshi <maha.yash@gmail.com>
2018-08-27 09:29:05 +0530

    Added call for GST

--- taxnewsbackend/routes/api/home.js
+++ taxnewsbackend/routes/api/home.js
@@ -4,10 +4,13 @@ var HomeDataResult = require('./homeDataResult');
 // Temporary we can hit Individual collection with Single API and then at the end would merge the result
 // SINGLE API > Muitple DB call > combine result > RESPONSE
 exports.getHomeDataList = function (req, res) {
-	HomeDataResult.getDataResult(1)
+	HomeDataResult.getDataResult(1) // Top stories
 		.then(function (result) {
 			dataResult.push(result);
-			return HomeDataResult.getDataResult(2);
+			return HomeDataResult.getDataResult(2); // Income Tax
+		}).then(function (result) {
+			dataResult.push(result);
+			return HomeDataResult.getDataResult(3); // GST
 		}).then(function (result) {
 			dataResult.push(result);
 			res.json({
