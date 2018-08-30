2dd01634a899fb23711db60da2d5e4167589e440
yashaswi maharshi <maha.yash@gmail.com>
2018-08-30 08:43:52 +0530

    console.log added for debug

--- taxnewsbackend/routes/api/home.js
+++ taxnewsbackend/routes/api/home.js
@@ -1,5 +1,5 @@
-let dataResult = [];
 var HomeDataResult = require('./homeDataResult');
+let dataResult = [];
 
 // Temporary we can hit Individual collection with Single API and then at the end would merge the result
 // SINGLE API > Muitple DB call > combine result > RESPONSE
@@ -7,15 +7,17 @@ exports.getHomeDataList = function (req, res) {
 	HomeDataResult.getDataResult(1) // Top stories
 		.then(function (result) {
 			dataResult.push(result);
+			console.log('Top stories ' + dataResult.length);
 			return HomeDataResult.getDataResult(2); // Income Tax
 		})
 		.then(function (result) {
 			dataResult.push(result);
+			console.log('IncomeTax ' + dataResult.length);
 			return HomeDataResult.getDataResult(3); // GST
 		})
 		.then(function (result) {
 			dataResult.push(result);
-			// res.send(200);
+			console.log('GST ' + dataResult.length);
 			res.json({
 				homeArticles: dataResult,
 			});
