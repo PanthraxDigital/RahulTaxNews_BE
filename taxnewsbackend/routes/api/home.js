5fd9dc6bef03c07b1d7d3b9d8a244ddfe1c28c4d
yashaswi maharshi <maha.yash@gmail.com>
2018-08-30 08:47:23 +0530

    local variable for storing the data

--- taxnewsbackend/routes/api/home.js
+++ taxnewsbackend/routes/api/home.js
@@ -1,23 +1,21 @@
 var HomeDataResult = require('./homeDataResult');
-let dataResult = [];
+
 
 // Temporary we can hit Individual collection with Single API and then at the end would merge the result
 // SINGLE API > Muitple DB call > combine result > RESPONSE
 exports.getHomeDataList = function (req, res) {
+	let dataResult = [];
 	HomeDataResult.getDataResult(1) // Top stories
 		.then(function (result) {
 			dataResult.push(result);
-			console.log('Top stories ' + dataResult.length);
 			return HomeDataResult.getDataResult(2); // Income Tax
 		})
 		.then(function (result) {
 			dataResult.push(result);
-			console.log('IncomeTax ' + dataResult.length);
 			return HomeDataResult.getDataResult(3); // GST
 		})
 		.then(function (result) {
 			dataResult.push(result);
-			console.log('GST ' + dataResult.length);
 			res.json({
 				homeArticles: dataResult,
 			});
