9a6e48d793b53aa89c25d358954c5ac6885dd552
yashaswi maharshi <maha.yash@gmail.com>
2018-09-13 08:57:21 +0530

    change the api path for detail page

--- taxnewsbackend/routes/api/articleById.js
+++ taxnewsbackend/routes/api/articleById.js
@@ -1,12 +1,25 @@
 var keystone = require('keystone');
 var articleList = keystone.list('Articles');
 
-exports.getArticleById = function (req, res) {
+exports.getArticleByIdAndCategory = function (req, res) {
 	console.log('category ' + req.params.category);
-	console.log('articleId ' + req.params.articleId);
 	articleList.model
 		.findOne({ _id: req.params.articleId })
-		.where({ category: req.params.category })
+		//.where({ category: req.params.category })
+		.populate('author', 'name') // Reference to User collection, and get only the Name of the User
+		.select('_id title subTitle articleDate author Image link description')
+		.exec(function (err, data) {
+			if (err) return res.json({ err: err });
+			res.json({
+				articles: data,
+			});
+		});
+};
+
+exports.getArticleById = function (req, res) {
+	console.log('article ' + req.params.articleId);
+	articleList.model
+		.find({ _id: req.params.articleId })
 		.populate('author', 'name') // Reference to User collection, and get only the Name of the User
 		.select('_id title subTitle articleDate author Image link description')
 		.exec(function (err, data) {
