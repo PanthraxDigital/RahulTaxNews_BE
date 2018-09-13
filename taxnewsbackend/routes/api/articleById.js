118d008f906af0ca911fd0b87d2a83f5fa5a11f3
yashaswi maharshi <maha.yash@gmail.com>
2018-09-13 08:18:20 +0530

    fix the api detail route

--- taxnewsbackend/routes/api/articleById.js
+++ taxnewsbackend/routes/api/articleById.js
@@ -2,9 +2,11 @@ var keystone = require('keystone');
 var articleList = keystone.list('Articles');
 
 exports.getArticleById = function (req, res) {
+	console.log('category ' + req.params.category);
 	console.log('articleId ' + req.params.articleId);
 	articleList.model
 		.findOne({ _id: req.params.articleId })
+		.where({ category: req.params.category })
 		.populate('author', 'name') // Reference to User collection, and get only the Name of the User
 		.select('_id title subTitle articleDate author Image link description')
 		.exec(function (err, data) {
