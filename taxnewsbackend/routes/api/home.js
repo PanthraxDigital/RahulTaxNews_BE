85167690e4ee304e0232c7f6238d11e0dc825645
yashaswi maharshi <maha.yash@gmail.com>
2018-10-19 18:34:35 +0530

    add complete link

--- taxnewsbackend/routes/api/home.js
+++ taxnewsbackend/routes/api/home.js
@@ -97,7 +97,7 @@ exports.getFeedDataList = function (req, res) {
 						title: data.title,
 						description: data.subTitle,
 						date: data.articleDate,
-						url: `${getArticleURL(index)}/${data._id}`,
+						url: `http://www.taxknowledge.in/${getArticleURL(index)}/${data._id}`,
 					});
 				})
 			);
