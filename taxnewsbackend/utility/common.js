87c52e52197be2b047b381192df3ab7e52781adc
yashaswi maharshi <maha.yash@gmail.com>
2018-12-10 05:18:40 +0530

    mapped menu categories with production data

--- taxnewsbackend/utility/common.js
+++ taxnewsbackend/utility/common.js
@@ -31,37 +31,40 @@ exports.getArticleURL = function (_index) {
 	}
 };
 
+// Production
 exports.getMenuMappingDev = function (_menuCategory) {
 	switch (parseInt(_menuCategory)) {
 		case 0: // from desk
-			return '5c09f2d2ad83e006a5cbd25d';
+			return '5c0da7b258920b125efbed47';
 		case 1: // top stories
-			return '5c07dd78cc1f9907914e002e';
+			return '5c0da7c858920b125efbed48';
 		case 2: // income tax
-			return '5c07ddafcc1f9907914e002f';
+			return '5c0da7dc58920b125efbed49';
 		case 3: // gst
-			return '5c07ddbccc1f9907914e0030';
+			return '5c0da7f758920b125efbed4a';
 		case 4: // vat-cst
-			return '5c07ddd0cc1f9907914e0031';
+			return '5c0da81358920b125efbed4b';
 		case 5: // excise
-			return '5c07ddf1cc1f9907914e0032';
+			return '5c0da82958920b125efbed4c';
 		case 6: // custom
-			return '5c07ddf1cc1f9907914e0033';
+			return '5c0da83a58920b125efbed4d';
 		case 7: // nbfc-rbi
-			return '5c07ddf1cc1f9907914e0034';
+			return '5c0da8bd58920b125efbed4e';
 		case 8: // sebi
-			return '5c07ddf1cc1f9907914e0035';
+			return '5c0da8cb58920b125efbed4f';
 		case 9: // roc-company-law
-			return '5c07ddf1cc1f9907914e0036';
+			return '5c0da8e658920b125efbed50';
 		case 10: // icai
-			return '5c07ddf1cc1f9907914e0037';
+			return '5c0da90358920b125efbed51';
 		case 11: // finance-budge
-			return '5c09f46337906706d192c052';
+			return '5c0da92b58920b125efbed52';
 		case 12: // general tax
-			return '5c09f47a37906706d192c053';
+			return '5c0da94058920b125efbed53';
 		case 13: // others
-			return '5c09f48837906706d192c054';
-		default :
+			return '5c0da95f58920b125efbed54';
+		case 14: // hidden Tab
+			return '5c0da97958920b125efbed55';
+		default:
 			return 'NA';
 	}
 };
